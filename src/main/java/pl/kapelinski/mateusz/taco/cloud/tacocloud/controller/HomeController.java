b9f869f956a527b565d165c486e6e3f02b3cdf94
Mateusz Kapelinski <mateuszkapelinski@gmail.com>
2022-06-21 17:30:00 +0200

    Initial application commit

---
+++ src/main/java/pl/kapelinski/mateusz/taco/cloud/tacocloud/controller/HomeController.java
@@ -0,0 +1,12 @@
+package pl.kapelinski.mateusz.taco.cloud.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
---
+++ src/test/java/pl/kapelinski/mateusz/taco/cloud/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kapelinski.mateusz.taco.cloud.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
